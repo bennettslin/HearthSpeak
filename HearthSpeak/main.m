a6fe0f58ec5a8d0690c569a69a2b4339308ac906
Bennett Lin <bennettslin@gmail.com>
2014-10-16 19:58:24 -0700

    Initial Commit

---
+++ HearthSpeak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HearthSpeak
+//
+//  Created by Bennett Lin on 10/16/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
